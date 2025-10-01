09205e290ca954a6b2c2197cc3c95572e2e0f030
aleksandra <matealeksandra@gmail.com>
2025-10-01 13:38:09 +0200

    Parse fetch response as JSON using await res.json() and assign to data

--- script.tsx
+++ script.tsx
@@ -8,22 +8,9 @@ const postsContainer = document.getElementById("posts-container");
 const fetchData = async () => {
     try {
         const res = await fetch(forumLatest);
-
+        const data = await res.json();
+        console.log(data);
     } catch (err) {
-
-    }
-};const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
-const forumTopicUrl = "https://forum.freecodecamp.org/t/";
-const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
-const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
-
-const postsContainer = document.getElementById("posts-container");
-
-const fetchData = async () => {
-    try {
-        const res = await fetch(forumLatest);
-
-    } catch (err) {
-
+        console.error(err);
     }
 };
