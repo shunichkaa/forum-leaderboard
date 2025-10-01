7e93dcbb65dae3ef44654c9a93b3c9954dedd5c5
aleksandra <matealeksandra@gmail.com>
2025-10-01 13:36:54 +0200

    Use await fetch with forumLatest inside try block and assign to res

--- script.tsx
+++ script.tsx
@@ -7,7 +7,23 @@ const postsContainer = document.getElementById("posts-container");
 
 const fetchData = async () => {
     try {
+        const res = await fetch(forumLatest);
+
     } catch (err) {
-        console.error(err);
+
+    }
+};const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
+const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
+const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
+
+const postsContainer = document.getElementById("posts-container");
+
+const fetchData = async () => {
+    try {
+        const res = await fetch(forumLatest);
+
+    } catch (err) {
+
     }
 };
