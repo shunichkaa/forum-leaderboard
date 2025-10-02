55fab910fad757df91028286cffbd510000262d8
aleksandra <matealeksandra@gmail.com>
2025-10-02 12:10:54 +0200

    Display number of replies in third td using ${posts_count - 1}

--- script.tsx
+++ script.tsx
@@ -39,13 +39,11 @@ const showLatestPosts = (data) => {
       <p class="post-title">${title}</p>
     </td>
     <td></td>
-    <td></td>
+    <td>${posts_count - 1}</td>
     <td></td>
     <td></td>
   </tr>
 `;
-        <td></td>
-        <td></td>
         <td></td>
         <td></td>
     </tr>`;
