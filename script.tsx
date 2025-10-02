42641b5abbcd6206a28c484f18ef694735749a8c
aleksandra <matealeksandra@gmail.com>
2025-10-02 11:31:41 +0200

    Create showLatestPosts arrow function with data parameter

--- script.tsx
+++ script.tsx
@@ -9,10 +9,13 @@ const fetchData = async () => {
     try {
         const res = await fetch(forumLatest);
         const data = await res.json();
-
     } catch (err) {
         console.log(err);
     }
 };
 
 fetchData();
+
+const showLatestPosts = (data) => {
+
+};
