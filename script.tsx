b3ec6a4f0fa0ef5022dced4ec84e348427d734ce
aleksandra <matealeksandra@gmail.com>
2025-10-02 12:08:57 +0200

    Add five empty td elements inside table row

--- script.tsx
+++ script.tsx
@@ -21,20 +21,42 @@ const showLatestPosts = (data) => {
     const { topic_list, users } = data;
     const { topics } = topic_list;
 
-    postsContainer.innerHTML = topics
-        .map((item) => {
-            const {
-                id,
-                title,
-                views,
-                posts_count,
-                slug,
-                posters,
-                category_id,
-                bumped_at,
-            } = item;
+    postsContainer.innerHTML = topics.map((item) => {
+        const {
+            id,
+            title,
+            views,
+            posts_count,
+            slug,
+            posters,
+            category_id,
+            bumped_at,
+        } = item;
 
-            return `<tr></tr>`;
-        })
-        .join("");
+        postsContainer.innerHTML = topics
+            .map((item) => {
+                const {
+                    id,
+                    title,
+                    views,
+                    posts_count,
+                    slug,
+                    posters,
+                    category_id,
+                    bumped_at,
+                } = item;
+
+                return `
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    `;
+            })
+            .join("");
+
+    }).join("");
 };
