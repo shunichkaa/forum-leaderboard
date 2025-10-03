eccefa863fa319c64597cffd90a7e4fe46739033
aleksandra <matealeksandra@gmail.com>
2025-10-03 12:16:16 +0200

    Add category 299 with name "Career Advice" and className "career" to allCategories

--- script.tsx
+++ script.tsx
@@ -4,7 +4,13 @@ const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
-const allCategories = {};
+
+const allCategories = {
+    299: {
+        category: "Career Advice",
+        className: "career"
+    }
+};
 
 const timeAgo = (time) => {
     const currentTime = new Date();
@@ -51,8 +57,8 @@ const fetchData = async () => {
 fetchData();
 
 const showLatestPosts = (data) => {
-    const { topic_list, users } = data;
-    const { topics } = topic_list;
+    const {topic_list, users} = data;
+    const {topics} = topic_list;
 
     postsContainer.innerHTML = topics.map((item) => {
         const {
